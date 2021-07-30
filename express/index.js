efb817c11e055f4977d6d0e48f85c82d23609679
Matt Sirkis <sirkis.matt@gmail.com>
2021-07-30 22:49:34 +1200

    endpoints pass tests and return tags

--- express/index.js
+++ express/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { getTopWords } = require("./utils/tags");
+const fs = require("fs");
 const cors = require("cors");
 const app = express();
-const rootPostDir = "./server/assets/posts";
+const rootPostDir = "../assets/posts";
+const readline = require("readline");
+const MarkdownIt = require("markdown-it"),
+  md = new MarkdownIt();
 
 app.use(cors());
 
@@ -15,7 +19,26 @@ app.use(cors());
  *  }
  * }
  */
-app.get("/post/:slug", function (req, res) {
+app.get("/post/:slug", async function (req, res) {
+  try {
+    const file = await fs.promises.readFile(
+      rootPostDir + "/" + req.params.slug + ".md",
+      "utf8"
+    );
+    const content = file.substring(file.indexOf("===\n", 1) + 4);
+
+    const tags = getTopWords(content);
+
+    res.status(200).json({
+      post: {
+        content,
+        tags,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+
   // ... fill in your own code ...
 });
 
@@ -29,9 +52,49 @@ app.get("/post/:slug", function (req, res) {
  *  ...
  * ]
  */
-app.get("/posts", function (req, res) {
-  res.send("Hello World");
-  // ... fill in you own code ...
+
+// Currently works for the happy path
+// Not full error handling
+// Needs to read meta data to check if file has the title and slug
+// Check what other errors might occur.. example: if(not .md) or if files.length === 0
+app.get("/posts", async function (req, res, next) {
+  try {
+    const files = await fs.promises.readdir(rootPostDir);
+    let index = files.length;
+
+    let posts = [];
+    for (const file of files) {
+      const readable = fs.createReadStream(rootPostDir + "/" + file);
+      const reader = readline.createInterface({ input: readable });
+      reader.on("line", (line) => {
+        if (line.substring(0, 6) === "Title:") {
+          posts.push({
+            title: line.substring(6).trim(),
+            slug: file.slice(0, -3),
+          });
+          reader.close();
+          readable.close();
+          index--;
+          if (index === 0) {
+            res.status(200).json(posts);
+          }
+        }
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { message: error.message, error };
+  }
+
+  res.status(500).json(response);
 });
 
 app.listen(8000, function () {
--- express/test/app.spec.js
+++ express/test/app.spec.js
@@ -1,9 +1,22 @@
 const app = require("../index");
 
 describe("App", () => {
-  it("GET / responds with 200 containing Hello World", async () => {
-    const response = await supertest(app).get("/posts");
-    expect(response.statusCode).to.equal(200);
-    expect(response.text).to.equal("Hello World");
+  describe("GET /posts", () => {
+    it("GET /posts responds with 200 containing json array of all post", async () => {
+      const response = await supertest(app).get("/posts");
+      expect(response.statusCode).to.equal(200);
+      expect(response.body[0]).to.have.property("title");
+      expect(response.body[0]).to.have.property("slug");
+      expect(response.body).to.be.an("array");
+    });
+  });
+
+  describe("GET /post/:slug", () => {
+    it("GET /posts responds with 200 containing json array of all post", async () => {
+      const response = await supertest(app).get("/post/kiasuism-vs-no8-wire");
+      expect(response.statusCode).to.equal(200);
+      expect(response.body.post).to.have.property("content");
+      expect(response.body.post).to.have.property("tags");
+    });
   });
 });
